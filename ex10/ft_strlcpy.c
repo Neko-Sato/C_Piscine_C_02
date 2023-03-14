42a91cf5f5280dfc4a2efc95fc5c9ea42f8e750c
hshimizu <hisatosi35350@gmail.com>
2023-03-14 17:31:07 +0900

    ヌルの時抜けるように

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 13:48:58 by satoneko          #+#    #+#             */
-/*   Updated: 2023/03/14 13:51:07 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/14 17:00:23 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	n;
 
 	n = ft_strlen(src);
-	while (1 < size--)
+	while (1 < size-- && *src)
 		*(dest++) = *(src++);
 	return (n);
 }
